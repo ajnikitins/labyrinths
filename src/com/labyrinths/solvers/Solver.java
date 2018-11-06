edda298ac96716669187fe272b24a7f6167ffda1
ajnikitins <ajnikitins@gmail.com>
2018-11-06 23:35:28 +0200

    Finish Solver abstract class

--- src/com/labyrinths/solvers/Solver.java
+++ src/com/labyrinths/solvers/Solver.java
@@ -1,5 +1,30 @@
 package com.labyrinths.solvers;
 
-public class Solver {
+import com.labyrinths.labyrinth.Labyrinth;
+import com.labyrinths.labyrinth.Node;
+import java.util.List;
+import java.util.Stack;
 
+public abstract class Solver {
+  private boolean solved;
+  List<Node> unvisitedNodes;
+  Stack<Node> path;
+
+  public boolean getSolved() {
+    return solved;
+  }
+
+  public List<Node> getUnvisitedNodes() {
+    return unvisitedNodes;
+  }
+
+  public Stack<Node> getPath() {
+    return path;
+  }
+
+  void setSolved(boolean solved) {
+    this.solved = solved;
+  }
+
+  public abstract void solveLabyrinth(Labyrinth labyrinth);
 }
